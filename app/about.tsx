52be07c15b2005dccdb18ae91a57e5e2ca382ab4
PedroFogl <pedro.pimentel@preve.g12.br>
2026-04-10 15:00:12 -0300

    first commit

---
+++ app/about.tsx
@@ -0,0 +1,30 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router';
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Segunda tela</Text>
+      <Link href="/" style={styles.button}>
+              Ir para tela inicial!
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+   container: {
+    flex: 1,
+    backgroundColor: '#12345e',
+    alignItems: 'center',
+    justifyContent: 'center',
+   },
+   text: {
+     color: '#fff',
+   },
+    button: {
+  fontSize: 20,
+  textDecorationLine: 'underline',
+  color: '#fff',
+}
+
+});
